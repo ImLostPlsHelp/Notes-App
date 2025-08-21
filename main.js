7bbe2dd85ee7cb8a726e75d279c78cb3109316c9
ImLostPlsHelp <maurits1005@gmail.com>
2025-08-21 15:18:13 +0700

    Finished with the delete button

--- main.js
+++ main.js
@@ -17,14 +17,15 @@ document.addEventListener("DOMContentLoaded", () => {
         //Kosongin dulu biar ga ada duplikasi
         notesList.innerHTML = '';
 
-        //Create elemen buat Itemnya, terus di dalam noteitem kita tambahin titel sama description
+        //Create elemen buat Itemnya, terus di dalam noteitem kita tambahin title sama description. buat button kita pakai class delete-button
         notes.forEach(note => {
             const noteItem = document.createElement("div");
             noteItem.classList.add("note-item");
             noteItem.innerHTML = `
                 <h3>${note.title}</h3>
                 <p>${note.description}</p>
-                `;
+                <button class="delete-button" data-note-id="${note.id}">Delete</button>
+                `; //tambahain data-node-id (data-* itu ngebuat custom data atribut) biar kita panggil nanti untuk cari id yang mau kita edit/delete/dll
             notesList.appendChild(noteItem);
         });
     }
@@ -39,7 +40,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     saveButton.addEventListener("click", () => {
         const title = document.getElementById("note-title").value;
-        const description = document.getElementById("note-description").value;
+        const description = document.getElementById("note-description").value;d
 
         const newNote = {
             id: Date.now(),
@@ -61,5 +62,16 @@ document.addEventListener("DOMContentLoaded", () => {
         //3. Ambil notes dari local storage dan ubah lagi ke javascript array
         //4. Push newNotes ke array notes
         //5. Simpan lagi ke dalam localstorage dengan bentuk string json
+    })
+
+    notesList.addEventListener("click", (event) => {
+        //Kita pasang event listener di notesList (event delegation) biar kita gak pasang di setiap tombol delete
+        if(event.target.classList.contains("delete-button")) { // Kalau yang dipencetnya tombol delete,
+            const noteToDelete = event.target.dataset.noteId; //kita ambil id pakai custom atribut data-* tadi
+            const notes = localStorage.getItem("notes") ? JSON.parse(localStorage.getItem("notes")) : []; //ambil notes kalau ada
+            const updatedNotes = notes.filter(note => note.id !== parseInt(noteToDelete)); // Kita filter dan buat array baru updatedNotes yang isinya semua note item selain yang sama dengan note.id yang dicari
+            localStorage.setItem("notes", JSON.stringify(updatedNotes)); // Kita simpan lagi ke localstorage jadi json string
+            getNotes();
+        }
     })
 })
